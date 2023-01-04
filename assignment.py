0c5e55229ed724becd43651a98f6065f5483f31d
zac milne <90716044+ye11owbucket@users.noreply.github.com>
2023-01-04 10:45:11 -0800

    waESD

--- assignment.py
+++ assignment.py
@@ -1,31 +1,25 @@
-from flask import Flask, request
+from flask import Flask,request
 from flask_cors import CORS
-import time, json
+import json
 
-app = Flask(__name__)  
-CORS(app)      
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/post",methods=["POST"])
+def postResponse():
+    # a simple echo server that response with the payload received.
+    payload = request.form
+    data = dict(payload)
+    print(data)
+    return json.dumps(data)
 
 
 
-@app.route("/json",methods=['POST'])
-def jsondata():
-    #url: http://127.0.0.1:5000/json
-    #only POST data is allowed
-    #data sent from ajax form request is retrieved 
-    #response is sent back json encoded
-    output = {
-        "greeting" : "Hello World",
-        "success" : 100,
-        "timestamp" : time.time()
-    }
-    x = request.form
-    x = dict(x)
-    output['payload'] = x
-    return json.dumps(output)
+
+app.run()
 
 
 
 
 
 
-app.run()          
